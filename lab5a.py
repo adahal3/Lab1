4000d2ea0a57b3fbf3d7f9371691e12f311bf3de
adahal3 <129253746+adahal3@users.noreply.github.com>
2024-02-12 14:21:09 -0500

    Add files via upload

---
+++ lab5a.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+
+def calculate_age():
+    birth_year = int(input("Enter your birth year: "))
+    current_year = datetime.now().year
+    age = current_year - birth_year
+    print("Your age is:", age)
+
+calculate_age()
